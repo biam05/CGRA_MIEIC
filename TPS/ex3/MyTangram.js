4d271d65c7b0a365dab1293c60a2863afd7c95c0
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-03-18 10:46:41 +0000

    added TP3

---
+++ TPS/ex3/MyTangram.js
@@ -0,0 +1,144 @@
+/**
+ * MyTangram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTangram extends CGFobject {
+	constructor(scene) {
+                super(scene);
+                this.diamond = new MyDiamond(scene);
+                this.triangle = new MyTriangle(scene);
+                this.parallelogram = new MyParallelogram(scene);
+                this.initBuffers();
+                this.initMaterials();
+	}
+
+        initMaterials(){
+                this.redMaterial = new CGFappearance(this.scene);
+                this.redMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.redMaterial.setDiffuse(1, 12/255, 12/255, 1.0);
+                this.redMaterial.setSpecular(0,1,0,1.0);
+                this.redMaterial.setShininess(10.0);  
+
+                this.greenMaterial = new CGFappearance(this.scene);
+                this.greenMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.greenMaterial.setDiffuse(0, 1, 0, 1.0);
+                this.greenMaterial.setSpecular(0,1,0,1.0);
+                this.greenMaterial.setShininess(10.0); 
+                
+                this.purpleMaterial = new CGFappearance(this.scene);
+                this.purpleMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.purpleMaterial.setDiffuse(171/255, 78/255, 195/255, 1.0);
+                this.purpleMaterial.setSpecular(0,1,0,1.0);
+                this.purpleMaterial.setShininess(10.0); 
+
+                this.orangeMaterial = new CGFappearance(this.scene);
+                this.orangeMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.orangeMaterial.setDiffuse(1, 157/255, 0, 1.0);
+                this.orangeMaterial.setSpecular(0,1,0,1.0);
+                this.orangeMaterial.setShininess(10.0); 
+
+                this.blueMaterial = new CGFappearance(this.scene);
+                this.blueMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.blueMaterial.setDiffuse(0, 157/255, 1, 1.0);
+                this.blueMaterial.setSpecular(0,1,0,1.0);
+                this.blueMaterial.setShininess(10.0); 
+
+                this.pinkMaterial = new CGFappearance(this.scene);
+                this.pinkMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.pinkMaterial.setDiffuse(1, 157/255, 211/255, 1.0);
+                this.pinkMaterial.setSpecular(0,1,0,1.0);
+                this.pinkMaterial.setShininess(10.0); 
+
+                this.yellowMaterial = new CGFappearance(this.scene);
+                this.yellowMaterial.setAmbient(0.2, 0.2, 0.2, 1.0);
+                this.yellowMaterial.setDiffuse(1, 1, 0, 1.0);
+                this.yellowMaterial.setSpecular(0,1,0,1.0);
+                this.yellowMaterial.setShininess(10.0); 
+        }
+
+	display(){
+
+        //GREEN DIAMOND -> Custom Material
+        var matriz2 = [1.0, 0.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0, 0.0,
+                    0.0, 0.0, 1.0, 0.0,
+                    -2.5, 0.5, 0.0, 1.0];
+                    
+        this.scene.pushMatrix();
+        this.scene.multMatrix(matriz2);
+        //this.greenMaterial.apply();
+        this.scene.materials[4].apply();
+        this.diamond.display();
+        this.scene.popMatrix();
+        
+        //PURPLE TRIANGLE
+        this.scene.pushMatrix();
+        this.scene.translate(-1,0,0);
+        this.scene.scale(2/Math.sqrt(8),2/Math.sqrt(8),2/Math.sqrt(8))
+        this.scene.rotate(Math.PI/4,0,0,1);
+        this.purpleMaterial.apply();
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        //RED TRIANGLE
+        this.scene.pushMatrix();
+        this.scene.translate(0,-1,0);
+        this.scene.scale(2/Math.sqrt(8),2/Math.sqrt(8),2/Math.sqrt(8))
+        this.scene.rotate(5*Math.PI/4,0,0,1);
+        this.redMaterial.apply();
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        //ORANGE TRIANGLE
+        this.scene.pushMatrix();
+        this.scene.translate(2,0,0);
+        this.scene.scale(4/Math.sqrt(8),4/Math.sqrt(8),4/Math.sqrt(8))
+        this.scene.rotate(-Math.PI/4,0,0,1);
+        this.orangeMaterial.apply();
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        //BLUE TRIANGLE
+        this.scene.pushMatrix();
+        this.scene.translate(2,0,0);
+        this.scene.scale(4/Math.sqrt(8),4/Math.sqrt(8),4/Math.sqrt(8))
+        this.scene.rotate(3*Math.PI/4,0,0,1);
+        this.blueMaterial.apply();
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        //PINK TRIANGLE
+        this.scene.pushMatrix();
+        this.scene.translate(3.5,2,0);
+        this.scene.scale(3/Math.sqrt(8),3/Math.sqrt(8),3/Math.sqrt(8))
+        this.scene.rotate(-3*Math.PI/4,0,0,1);
+        this.pinkMaterial.apply();
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        //YELLOW PARALLELOGRAM
+        this.scene.pushMatrix();
+        this.scene.translate(2,2,0);
+        this.scene.scale(-1,1,1)
+        this.yellowMaterial.apply();
+        this.parallelogram.display();
+        this.scene.popMatrix();
+        }
+        
+        updateBuffers(complexity){
+
+        }
+
+        enableNormalViz(){
+                this.diamond.enableNormalViz();
+                this.parallelogram.enableNormalViz();
+                this.triangle.enableNormalViz();
+        }
+
+        disableNormalViz(){
+                this.diamond.disableNormalViz();
+                this.parallelogram.disableNormalViz();
+                this.triangle.disableNormalViz();
+        }
+}
---
+++ TPS/ex3/MyUnitCube.js
@@ -0,0 +1,134 @@
+/**
+ * MyUnitCube
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyUnitCube extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			//cada vertice do cubo tem 3 normais, uma para cada eixo
+			//logo sera preciso definir cada vertice 3 vezes
+
+			//0
+			0.5, 0.5, 0.5,   //0
+			0.5, 0.5, 0.5,   //1
+			0.5, 0.5, 0.5,   //2
+			//1
+			0.5, -0.5, 0.5,  //3
+			0.5, -0.5, 0.5,  //4
+			0.5, -0.5, 0.5,  //5
+			//2
+			-0.5, 0.5, 0.5,	 //6
+			-0.5, 0.5, 0.5,	 //7
+			-0.5, 0.5, 0.5,	 //8
+			//3
+			-0.5, -0.5, 0.5, //9
+			-0.5, -0.5, 0.5, //10
+			-0.5, -0.5, 0.5, //11
+			//4
+			0.5, 0.5, -0.5,  //12
+			0.5, 0.5, -0.5,  //13
+			0.5, 0.5, -0.5,  //14
+			//5
+			0.5, -0.5, -0.5, //15
+			0.5, -0.5, -0.5, //16
+			0.5, -0.5, -0.5, //17
+			//6
+			-0.5, 0.5, -0.5, //18
+			-0.5, 0.5, -0.5, //19
+			-0.5, 0.5, -0.5, //20
+			//7
+			-0.5, -0.5, -0.5,//21
+			-0.5, -0.5, -0.5,//22
+			-0.5, -0.5, -0.5,//23
+		];
+
+		this.indices = [
+			// --- 0123
+			//0, 2, 3,
+			0, 6, 9,
+			//0, 3, 1,
+			0, 9, 3,
+
+			// --- 4567
+			//4, 5, 7,
+			12, 15, 21,
+			//4, 7, 6,
+			12, 21, 18,
+
+			// --- 1345
+			//4, 0, 1,
+			13, 1, 4,
+			//4, 1, 5,
+			13, 4, 16,
+
+			// --- 2367
+			//6, 7, 3,
+			19, 22, 10,
+			//6, 3, 2,
+			19, 10, 7,
+
+			// --- 1357
+			//7, 5, 1,
+			23, 17, 5,
+			//7, 1, 3,
+			23, 5, 11,
+
+			// --- 0246
+			//4, 6, 2,
+			14, 20, 8,
+			//4, 2, 0
+			14, 8, 2
+		    
+		];
+
+		this.normals = [
+			//uma normal para cada eixo em cada vertice -> 3 * 8 = 24 normais
+			//definir as normais de cada vertice pela ordem em que eles foram definidos
+			
+			//x, y, z
+			0, 0, 1,
+            1, 0, 0,
+            0, 1, 0,
+			//x, -y, z
+			0, 0, 1,
+            1, 0, 0,
+            0, -1, 0,
+			//-x, y, z
+			0, 0, 1,
+            -1, 0, 0,
+            0, 1, 0,
+			//-x, -y, z
+			0, 0, 1,
+            -1, 0, 0,
+            0, -1, 0,
+			//x, y, -z
+			0, 0, -1,
+            1, 0, 0,
+            0, 1, 0,
+			//x, -y, -z
+			0, 0, -1,
+            1, 0, 0,
+            0, -1, 0,
+			//-x, y, -z
+			0, 0, -1,
+            -1, 0, 0,
+            0, 1, 0,
+			//-x, -y, -z
+            0, 0, -1
+            -1, 0, 0,
+            0, -1, 0,
+        ];
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+
+	updateBuffers(complexity){
+        this.initBuffers();
+        this.initNormalVizBuffers();
+    }
+}
